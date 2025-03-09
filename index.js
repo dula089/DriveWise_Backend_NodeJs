458192801ad45a6cabf89c5a7a1f00517ff86f1a
Dulani Kariyawasam <dulani.20231280@iit.ac.lk>
2025-03-09 16:44:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.json({ message: "Node.js Backend for Flutter is Running!" });
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`✅ Server running on http://localhost:${PORT}`);
+});
