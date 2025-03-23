5a6fbf0bf218fe947d0360b5c433eb350aa4722d
chaniruM <chanirumannapperuma18@gmail.com>
2025-03-23 12:31:37 +0530

    changed origin for hosting

--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const routes = require("./routes/routes");
 const path = require("path");
 const connectDB = require("./config/db");
 
 // Load environment variables
 dotenv.config();
-connectDB(); // Connect to MongoDB
+connectDB(); 
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+// app.use(cors({ origin: "http://localhost:3000" }));
+// app.use(cors({
+//   origin: 'http://10.0.2.2:5001/', // Replace with your Flutter app's URL
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
+app.use(cors({
+  origin: ["http://10.0.2.2:5001", "http://localhost:5001", "http://localhost:3000"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api", require("./routes/routes"));
 app.use("/api/user", require("./routes/userRoutes"));
-
-// Assuming 'app' is your Express application instance
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Error Handling Middleware
 const errorHandler = require("./middleware/errorHandler");
 app.use(errorHandler);
 
-// app.use("/api", routes);
-
 // Start Server
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5001;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
