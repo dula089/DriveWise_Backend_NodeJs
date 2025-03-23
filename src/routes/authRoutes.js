96052c41838520e1f1dafd6757621c7c5c0798e0
M . H . Disara Sewmini <163980937+disara03@users.noreply.github.com>
2025-03-23 18:20:53 +0530

    Update authRoutes.js
    adding change password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,3 +1,15 @@
+// const express = require("express");
+// const authController = require("../controllers/authController");
+// const authMiddleware = require("../middleware/authMiddleware"); 
+
+// const router = express.Router();
+
+// router.post("/register", authController.register);
+// router.post("/login", authController.login);
+// router.get("/protected-route", authMiddleware, authController.protectedRoute); 
+// router.post("/logout", authMiddleware, authController.logout); 
+
+// module.exports = router;
 const express = require("express");
 const authController = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware"); 
@@ -8,5 +20,6 @@ router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/protected-route", authMiddleware, authController.protectedRoute); 
 router.post("/logout", authMiddleware, authController.logout); 
+router.post("/change-password", authMiddleware, authController.changePassword);
 
 module.exports = router;
