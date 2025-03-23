baf686de89d87216984ff4407fba5379078d0d95
M . H . Disara Sewmini <163980937+disara03@users.noreply.github.com>
2025-03-23 18:22:40 +0530

    Update authRoutes.js
    updated code

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,3 @@
-// const express = require("express");
-// const authController = require("../controllers/authController");
-// const authMiddleware = require("../middleware/authMiddleware"); 
-
-// const router = express.Router();
-
-// router.post("/register", authController.register);
-// router.post("/login", authController.login);
-// router.get("/protected-route", authMiddleware, authController.protectedRoute); 
-// router.post("/logout", authMiddleware, authController.logout); 
-
-// module.exports = router;
 const express = require("express");
 const authController = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware"); 
